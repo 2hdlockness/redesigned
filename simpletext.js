45a831c1f08ab4c28ac958c2e86a276ef1a8fdfc
Horia Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-10-22 19:07:36 +0300

    Add files via upload

---
+++ simpletext.js
@@ -0,0 +1,38 @@
+/* Magic Mirror
+ *
+ * MIT Licensed.
+ *
+ * Redesigned by Răzvan Cristea
+ * for iPad 3 & HD display
+ * https://github.com/hangorazvan
+ */
+Module.register("simpletext",{
+
+	defaults: {
+		text: "", 		// text with or without HTML tag
+		cssClass: "",	// css style for text or HTML tag
+	},
+
+	getScripts: function() {
+	    return false;
+	},
+	
+	getStyles: function() {
+	    return ["font-awesome.css"];
+	},
+
+	start: function() {
+		Log.info("Starting module: " + this.name); 
+	},
+
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.className = this.config.cssClass;
+		wrapper.innerHTML = this.config.text;
+		return wrapper;
+	},
+
+	getHeader: function () {
+		return this.data.header;
+	},
+});
